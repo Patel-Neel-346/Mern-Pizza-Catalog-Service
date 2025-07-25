bb5552e292e39004d5580f78741aea9c13f957af
developerdesigner18 <developer.designer18@gmail.com>
2025-07-25 10:38:25 +0530

    add:added Configuration setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
+import config from "config";
+
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.json({ Port: config.get("server.port"), Message: "Helllo World!" });
 });
 
 app.use(globalErrorHandler);
