c059d3a3147bb2b45c3a4f17b6eb52ec399ec43e
developerdesigner18 <developer.designer18@gmail.com>
2025-07-25 11:42:31 +0530

    add:added mongoDb Connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,8 @@
+import mongoose from "mongoose";
+import config from "config";
+import logger from "./logger";
 
+export const initDb = async () => {
+    await mongoose.connect(config.get("database.url"));
+    logger.info("Database is Connected");
+};
